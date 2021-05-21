a7a9bb1737702822894b1bbe408d65a3fa38342d
SAKATA-Atsuki <hhnkkr@icloud.com>
2021-05-21 20:52:28 +0900

    投稿のテーブルとモデルを作成

---
+++ database/migrations/2021_05_21_114125_create_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id')->comment('投稿ID');
+            $table->integer('user_id')->comment('ユーザーID');
+            $table->text('image')->comment('画像');
+            $table->text('message')->nullable($value = true)->comment('メッセージ');
+            $table->timestamp('created_at')->nullable($value = true)->comment('登録日時');
+            $table->timestamp('updated_at')->nullable($value = true)->comment('更新日時');
+            $table->softDeletes()->comment('削除日時');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
